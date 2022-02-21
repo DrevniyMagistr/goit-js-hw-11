2c283e7298ff7a972cf285a881d16771d14b8ee2
DrevniyMagistr <sasha.klishch@gmail.com>
2022-02-21 10:38:59 +0200

    fix load more

--- src/js/utils/actionFunction.js
+++ src/js/utils/actionFunction.js
@@ -16,12 +16,12 @@ export const pagesCount = () => {
 
 export const makeActiveButton = () => {
   pagesCount() > dataApi.page - 1
-    ? refs.moreBtn.classList.add('is-hidden')
-    : refs.moreBtn.classList.remove('is-hidden');
+    ? refs.moreBtn.classList.remove('is-hidden')
+    : refs.moreBtn.classList.add('is-hidden');
 };
 
 export const noMoreResource = () => {
-  if (pagesCount() === dataApi.page - 1) {
+  if (pagesCount() === dataApi.page) {
     return endMessage();
   }
 };
